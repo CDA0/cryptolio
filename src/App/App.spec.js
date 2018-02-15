543478d48e57c01bdcdcbd1c09acbdbf4688ad0f
Rob Howell <rob@robhowell.com>
2018-02-15 22:53:16 +0000

    Add shallow rendering example, fixes #10

--- src/App/App.spec.js
+++ src/App/App.spec.js
@@ -1,10 +1,21 @@
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from './App';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
-it('renders correctly', () => {
-  const tree = renderer.create(<App />);
+const getShallowRenderer = toRender => {
+  const renderer = new ShallowRenderer();
+  renderer.render(toRender);
+  return renderer;
+};
+
+const getShallowOutput = toRender =>
+  getShallowRenderer(toRender).getRenderOutput();
+
+it('renders', () => {
+  const rendered = getShallowOutput(<App />);
+
+  expect(rendered).toMatchSnapshot();
 });
